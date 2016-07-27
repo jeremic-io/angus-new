39f68e1f79504275cce717242935aef8fdbb417c
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-27 11:26:53 +0200

    Create index.php page. Add jQuery and Bootstrap. Prepare for deploying on Heroku.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="pest control, ants, rats, mice, exterminator, bugs, pests, roaches, cockroaches, fire ants, mosquitoes, termite, fly misting, rodent, rodents, bugs, exterminators, extermination, palm city, stuart, port st lucie, hobe sound, tequesta, and jupiter, fl ">
+  <meta name="description" content="Angus Pest Control provides pest control service to homes and businesses in Palm City, Stuart, Port St Lucie, Hobe Sound, Tequesta, and Jupiter, FL.">
+  <title>Angus Termite and Pest Control</title>
+  <!-- CSS Stylesheets -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- HTML5 Shiv for older browsers -->
+  <!--[if lt IE 9]>
+    <script src="js/vendor/html5shiv.js"></script>
+  <![endif]-->
+</head>
+  <body>
+    <!-- Contact Us -->
+    <section class="contact-us">
+      <h2 class="section-h2">Contact Us</h2>
+    </section>
+
+    <!-- Javascript scripts and libs -->
+    <script src="js/jquery-1.12.4.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
