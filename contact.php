64fde16d2ccbb260438e58bdb60988e5734bb01f
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-28 17:11:48 +0200

    Prepare for upload

--- contact.php
+++ contact.php
@@ -1,6 +1,15 @@
 <?php
   if(isset($_POST['name'])) {
-    sleep(5);
-    echo 'The message was sent!';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $msg = "Name: {$name}\nEmail: {$email}\nPhone: {$phone}\nMessage: \n" . $message;
+
+    if(mail("draganjeremic01.com", "Contact Us", $msg)) {
+      echo 'The message was send!';
+    }
+  } else {
+    echo 'Please enter all info!';
   }
 ?>
